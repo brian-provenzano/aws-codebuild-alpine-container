ad0033be9484e8c3b4ccd471cf6b36bfe215d973
Brian Provenzano <bproven@gmail.com>
2019-02-04 12:25:42 -0700

    fix: shebang /bin/sh ya dummy - this is alpine linux!

--- get-ami.sh
+++ get-ami.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 export NEW_AMI=$(jq -r '.builds[].artifact_id' manifest.json | cut -d':' -f2)
 echo "From Packer post processor - the AMI created: ${NEW_AMI}"
 echo "Capturing AMI in ami.txt locally..."
