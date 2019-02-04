b8777d998b5e919a0660f344468dc93ff9b55ebc
Brian Provenzano <bproven@gmail.com>
2019-02-04 15:21:03 -0700

    fix: remove profile from cli calls

--- cleanup-ami.sh
+++ cleanup-ami.sh
@@ -4,9 +4,9 @@
 # since we are running this after each build and started with 1 image, should always retain 2
 #
 
-AMI_COUNT=$(aws ec2 describe-images --owners self --filters 'Name=tag:Purpose,Values=web' --profile hostingdemo-fulladmin --query 'Images[*].{ID:ImageId}' --output text | wc -l)
+AMI_COUNT=$(aws ec2 describe-images --owners self --filters 'Name=tag:Purpose,Values=web' --query 'Images[*].{ID:ImageId}' --output text | wc -l)
 if [[$AMICOUNT -gt 1]]; then
-    aws ec2 describe-images --owners self --filters 'Name=tag:Purpose,Values=web' --profile hostingdemo-fulladmin --query 'sort_by(Images, &CreationDate)[0].[ImageId]' --output text > /root/ami-delete.txt
+    aws ec2 describe-images --owners self --filters 'Name=tag:Purpose,Values=web' --query 'sort_by(Images, &CreationDate)[0].[ImageId]' --output text > /root/ami-delete.txt
     aws ec2 describe-images --image-ids $(cat /root/ami-delete.txt) | jq -r '.Images[].BlockDeviceMappings[0].Ebs.SnapshotId' > /root/snapami-delete.txt
     echo -e "Following are the snapshots associated with ami: $(cat /root/snapami-delete.txt) \n "
     echo -e "Deregister the AMI: $(cat /root/ami-delete.txt) \n"
