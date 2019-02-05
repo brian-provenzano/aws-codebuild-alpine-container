9e4346d76840a60e38bdc58622a85d133f00ebac
Brian Provenzano <bproven@gmail.com>
2019-02-05 09:46:57 -0700

    docs: add docs/usage to script

--- get-ami.sh
+++ get-ami.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+#
+# Gets current AMI (post processor in packer in aws codebuild) by reading the packer manifest.json created in packer json template
+# Captures the image ID in local file for later usage in the build process
+#
+# Usage:
+# get-ami.sh 
+#
+# Notes:
+#
+# Author:
+# BJP - 1/19
+#
 export NEW_AMI=$(jq -r '.builds[].artifact_id' manifest.json | cut -d':' -f2)
 echo "From Packer post processor - the AMI created: ${NEW_AMI}"
 echo "Capturing AMI in ami.txt locally..."
